65dfb5246bf32887c6da404364aa74bc81fc7d93
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-05-12 19:17:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0, 2);
+};
 
-function returnFirstTwoDriver(driver) {
-  const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
-  return 
-}
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(timesFare) {
+  return function (fare) {
+    return timesFare * fare;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(drivers, whosDriving) {
+  return whosDriving(drivers);
+};
